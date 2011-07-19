04c355efc8a37aa8d39bcbdbc5196bffbdb37e15
Alexander Bigga <linux@bigga.de>
2011-07-19 20:12:24 +0000

    - add updated ext-emconf.php to SVN

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "ab_booking".
 #
-# Auto generated 06-06-2011 22:25
+# Auto generated 19-07-2011 22:11
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array(
 Another view is an availability check.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.4.4',
+	'version' => '0.4.5',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ Another view is an availability check.',
 			'date2cal' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:33:{s:9:"ChangeLog";s:4:"a57f";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"b12e";s:12:"ext_icon.gif";s:4:"787f";s:17:"ext_localconf.php";s:4:"38ec";s:14:"ext_tables.php";s:4:"bb8e";s:14:"ext_tables.sql";s:4:"df2a";s:15:"flexform_ds.xml";s:4:"b347";s:20:"flexform_ds.xml.orig";s:4:"571a";s:19:"flexform_ds.xml.rej";s:4:"ab89";s:29:"icon_tx_abbooking_booking.gif";s:4:"475a";s:26:"icon_tx_abbooking_item.gif";s:4:"d00b";s:27:"icon_tx_abbooking_price.png";s:4:"2d34";s:27:"icon_tx_abbooking_price.xcf";s:4:"1c86";s:29:"icon_tx_abbooking_product.gif";s:4:"475a";s:29:"icon_tx_abbooking_seasons.png";s:4:"9698";s:29:"icon_tx_abbooking_seasons.xcf";s:4:"ba5b";s:25:"locallang_csh_product.xml";s:4:"3089";s:16:"locallang_db.xml";s:4:"88d5";s:17:"locallang_tca.xml";s:4:"83b3";s:22:"locallang_tca.xml.orig";s:4:"b8bb";s:21:"locallang_tca.xml.rej";s:4:"e70e";s:7:"tca.php";s:4:"d347";s:14:"doc/manual.sxw";s:4:"99d9";s:19:"doc/wizard_form.dat";s:4:"112b";s:20:"doc/wizard_form.html";s:4:"4f72";s:30:"lib/class.tx_abbooking_div.php";s:4:"331d";s:33:"lib/class.tx_abbooking_remote.php";s:4:"7fab";s:30:"pi1/class.tx_abbooking_pi1.php";s:4:"b45b";s:17:"pi1/locallang.xml";s:4:"3199";s:18:"res/cssBooking.css";s:4:"9559";s:20:"static/constants.txt";s:4:"cd51";s:16:"static/setup.txt";s:4:"2f65";}',
+	'_md5_values_when_last_written' => 'a:43:{s:9:"ChangeLog";s:4:"a57f";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"b12e";s:12:"ext_icon.gif";s:4:"787f";s:17:"ext_localconf.php";s:4:"38ec";s:14:"ext_tables.php";s:4:"bb8e";s:14:"ext_tables.sql";s:4:"da0a";s:15:"flexform_ds.xml";s:4:"6f0e";s:20:"flexform_ds.xml.orig";s:4:"571a";s:19:"flexform_ds.xml.rej";s:4:"ab89";s:29:"icon_tx_abbooking_booking.gif";s:4:"475a";s:26:"icon_tx_abbooking_item.gif";s:4:"d00b";s:27:"icon_tx_abbooking_price.png";s:4:"2d34";s:27:"icon_tx_abbooking_price.xcf";s:4:"1c86";s:29:"icon_tx_abbooking_product.gif";s:4:"475a";s:29:"icon_tx_abbooking_seasons.png";s:4:"9698";s:29:"icon_tx_abbooking_seasons.xcf";s:4:"ba5b";s:25:"locallang_csh_product.xml";s:4:"3089";s:16:"locallang_db.xml";s:4:"048b";s:17:"locallang_tca.xml";s:4:"4b03";s:22:"locallang_tca.xml.orig";s:4:"b8bb";s:21:"locallang_tca.xml.rej";s:4:"e70e";s:7:"tca.php";s:4:"fd3f";s:49:"doc/ab_booking_0_4_0_preise_und_saison_zeiten.jpg";s:4:"adb7";s:14:"doc/manual.pdf";s:4:"8fe1";s:14:"doc/manual.sxw";s:4:"146f";s:37:"doc/screenshot_ab_booking_backend.jpg";s:4:"06d5";s:46:"doc/screenshot_ab_booking_backend_flexform.jpg";s:4:"97bb";s:46:"doc/screenshot_ab_booking_backend_listview.jpg";s:4:"d963";s:53:"doc/screenshot_ab_booking_backend_plugin_settings.jpg";s:4:"2282";s:56:"doc/screenshot_ab_booking_frontend_belegungskalender.jpg";s:4:"f553";s:55:"doc/screenshot_ab_booking_frontend_buchungsformular.jpg";s:4:"8e5c";s:59:"doc/screenshot_ab_booking_frontend_verfuegbarkeit_liste.jpg";s:4:"d9d8";s:62:"doc/screenshot_ab_booking_frontend_verfuegbarkeitspruefung.jpg";s:4:"208a";s:19:"doc/wizard_form.dat";s:4:"112b";s:20:"doc/wizard_form.html";s:4:"4f72";s:30:"lib/class.tx_abbooking_div.php";s:4:"444c";s:33:"lib/class.tx_abbooking_remote.php";s:4:"7fab";s:30:"pi1/class.tx_abbooking_pi1.php";s:4:"d03c";s:17:"pi1/locallang.xml";s:4:"8f2b";s:18:"res/cssBooking.css";s:4:"9559";s:20:"static/constants.txt";s:4:"cd51";s:16:"static/setup.txt";s:4:"2f65";}',
 	'suggests' => array(
 	),
 );
